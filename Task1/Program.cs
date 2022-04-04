d6c5ff8fe5227c931e57cce2e22c330873c6b2d0
abotnari80 <abotnari80@gmail.com>
2022-04-04 17:20:05 +0300

    Задача 1

---
+++ Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿System.Console.WriteLine("Введите количество строк и столбцов");
+int n = Convert.ToInt32(Console.ReadLine()); 
+int m = Convert.ToInt32(Console.ReadLine()); 
+double[,] array = new double[m,n];
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+      array[i,j] = Math.Round(new Random().Next(-10,10) + new Random().NextDouble(), 1);  
+      System.Console.Write(array[i,j] + " ");
+    }
+    System.Console.WriteLine();
+}
