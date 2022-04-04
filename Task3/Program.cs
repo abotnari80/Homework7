68fbf6e65f14167c3f311d7885a4794ba02f0738
abotnari80 <abotnari80@gmail.com>
2022-04-04 17:20:56 +0300

    Задача 3

---
+++ Task3/Program.cs
@@ -0,0 +1,40 @@
+﻿int[,] array = new int[3,4];
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 4; j++)
+    {
+        array[i,j] = new Random().Next(0,10);
+        System.Console.Write(array[i,j] + " ");
+    }
+    System.Console.WriteLine();
+}
+System.Console.Write("Среднее арифметическое ");
+double sa;
+double sum = 0;
+for (int i = 0; i < 3; i++)
+{
+    sum = array[i,0] + sum;
+}
+sa = sum/3;
+System.Console.Write($"{Math.Round(sa,2)} ");
+sum = 0;
+for (int i = 0; i < 3; i++)
+{
+    sum = array[i,1] + sum;
+}
+sa = sum/3;
+System.Console.Write($"{Math.Round(sa,2)} ");
+sum = 0;
+for (int i = 0; i < 3; i++)
+{
+    sum = array[i,2] + sum;
+}
+sa = sum/3;
+System.Console.Write($"{Math.Round(sa,2)} ");
+sum = 0;
+for (int i = 0; i < 3; i++)
+{
+    sum = array[i,3] + sum;
+}
+sa = sum/3;
+System.Console.Write($"{Math.Round(sa,2)} ");
